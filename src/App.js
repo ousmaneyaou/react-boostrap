7cfdda70f0a062d55a209335fd54275b397b1ee2
ousmane <ousmaneyaou00@gmail.com>
2024-06-05 12:42:53 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar, Container, Nav, Card, CardDeck } from 'react-bootstrap';
+import Form from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
